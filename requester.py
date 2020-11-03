0ca25d795eea00dbfef7a8f8f71e02eeae654ee7
GobiasSomeCoffeeCo <60927833+GobiasSomeCoffeeCo@users.noreply.github.com>
2020-11-02 19:37:16 -0500

    Helper file upload

---
+++ requester.py
@@ -0,0 +1,85 @@
+import bs4
+import requests
+import re
+import tqdm
+
+
+
+class NewsHead:
+    def __init__(self):
+        pass
+
+        # self.links = []
+        # self.headlines = []
+
+    # TODO: A lot of DRY code. See about making it one function and passing in a dict or JSON?
+
+    def get_nytimes_headlines(self):
+        resp = requests.get("https://www.nytimes.com/")
+        soup = bs4.BeautifulSoup(resp.text, "lxml")
+        headline = soup.find_all("h2", class_="css-1ayculb e1voiwgp0")
+        a_tags = soup.find_all('div', class_='css-6p6lnl')
+        links = []
+        headlines = []
+        for article in headline:
+        #     link = article.a.get("href")
+            article = article.text
+            headlines.append(article)
+            # links.append(link)
+        for tag in a_tags:
+            tag = tag.a.get('href')
+            links.append(tag)
+        print(headlines, links)
+
+    def get_washpost_headlines(self):
+        resp = requests.get("https://www.washingtonpost.com/")
+        soup = bs4.BeautifulSoup(resp.text, "lxml")
+        headline = soup.find_all("h2", class_=(re.compile("headline")), limit=12)
+        links = []
+        headlines = []
+        try:
+            for article in headline:
+                link = article.a.get("href")
+                article = article.text.replace(u"\xa0", u" ")
+                headlines.append(article)
+                links.append(link)
+        except AttributeError:
+            pass
+        return headlines, links
+
+    def get_atlantic_headlines(self):
+        resp = requests.get("https://www.theatlantic.com/")
+        soup = bs4.BeautifulSoup(resp.text, "lxml")
+        headline = soup.find_all("a", class_=(re.compile("hed-link")))
+        links = []
+        headlines = []  # [main_headline]
+        try:
+            for article in headline:
+                link = article.get("href")
+                article = article.text
+                headlines.append(article)
+                links.append(link)
+        except AttributeError:
+            pass
+        return headlines, links
+
+    def get_politico_headlines(self):
+        resp = requests.get("https://www.politico.com/")
+        soup = bs4.BeautifulSoup(resp.text, "lxml")
+        headline = soup.find_all("h3", class_="headline")
+        links = []
+        headlines = []
+        try:
+            for article in headline:
+                link = article.a.get("href")
+                article = article.text
+                article = re.sub(r"\s+", " ", article)
+                headlines.append(article)
+                links.append(link)
+        except AttributeError:
+            pass
+
+        return headlines, links
+
+news_head = NewsHead()
+news_head.get_nytimes_headlines()
